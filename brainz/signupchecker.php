b1696c68a3ed01bf4dedaff62a96694b70c14dcf
Klabin <calvinw1103@gmail.com>
2023-09-27 10:47:28 +0700

    commit

--- brainz/signupchecker.php
+++ brainz/signupchecker.php
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/navbar.css">
+</head>
+
 <?php
 
     $exists = false;
